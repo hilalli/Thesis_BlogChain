c8672793913870f62d1e3d2bc44c2b4ddaf63d85
Hilal <121344853+hilalli@users.noreply.github.com>
2024-06-09 15:06:01 +0300

    Add files via upload

--- copy_idl.js
+++ copy_idl.js
@@ -1,4 +1,4 @@
 const fs = require("fs");
-const blog_idl = require("./target/idl/blog_sol.json");
+const blog_idl = require("./target/deploy/blog_sol-keypair.json");
 
 fs.writeFileSync("./app/src/idl.json", JSON.stringify(blog_idl, null, 2));
